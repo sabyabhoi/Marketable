d306cb6bc12524abe6f98d9e85a26a0ab8704927
sabyabhoi <sabyabhoi@gmail.com>
2020-11-09 13:38:12 +0530

    Added middlewares for error handling

--- server/index.js
+++ server/index.js
@@ -3,9 +3,25 @@ const cors = require("cors");
 const helmet = require("helmet");
 const morgan = require("morgan");
 
+const middlewares = require("./middlewares");
+
+require("dotenv").config();
+
 const app = express();
 
 app.use(cors());
 app.use(helmet());
 app.use(morgan("tiny"));
 app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.json({ message: "Marketable" });
+});
+
+app.use(middlewares.notFound);
+app.use(middlewares.errorHandler);
+
+const port = process.env.PORT || 8000;
+app.listen(port, () => {
+  console.log(`Listening on http://localhost:${port}`);
+});
